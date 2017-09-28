9e0dda6a81b293ed8fd5d84dff50c97d17323bfb
小林純也 <atsubeee.ngzk@gmail.com>
2017-09-28 21:59:39 +0900

    新ファイル追加、uiを変更する

---
+++ musicApp/postUrlViewController.swift
@@ -0,0 +1,24 @@
+//
+//  postUrlViewController.swift
+//  musicApp
+//
+//  Created by 小林純也 on 2017/09/28.
+//  Copyright © 2017年 Junya Kobayashi. All rights reserved.
+//
+
+import UIKit
+
+class postUrlViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
