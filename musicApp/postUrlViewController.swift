719c238cbff76ae0817b60523a1b15aa8c661417
小林純也 <atsubeee.ngzk@gmail.com>
2017-09-29 13:20:31 +0900

    改善中

--- musicApp/postUrlViewController.swift
+++ musicApp/postUrlViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class postUrlViewController: UIViewController {
 
@@ -19,6 +20,18 @@ class postUrlViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func add(_ sender: AnyObject) {
+        let DBRef:DatabaseReference = Database.database().reference()
+        let url: String = String(UITextField.text!)
+        let data = ["url": url]
+        if(data == null){
+            DBRef.child("user/01").setValue(data)
+        }else{
+            DBRef.child("user/01").pushValue(data)
+        }
+        
     }
     
 }
